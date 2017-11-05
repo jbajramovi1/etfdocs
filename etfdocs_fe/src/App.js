bf2d54dee286ad301575eb47b2540e6d14dfd3a1
ohri31 <mirza.ohranovic@gmail.com>
2017-11-05 13:14:12 +0100

    Landing page created

--- etfdocs_fe/src/App.js
+++ etfdocs_fe/src/App.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import Cover from './Cover';
+import Menu from './Menu';
+import About from './About';
+import Footer from './Footer';
+
+const App = () => {
+  return (
+    <div className="etfdocs">
+      <Menu />
+      <Cover />
+      <About />
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
